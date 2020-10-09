a26d412bd9d878d256b471a815e9b45cdda5a34c
An Doan <dtan42@gmail.com>
2020-10-09 13:04:46 +1000

    base project

---
+++ src/Video.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Video.css'
+function Video() {
+    return (
+        <div className="video">
+            <h1>Videos here</h1>
+        </div>
+    )
+}
+
+export default Video
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB5yIXEZ6Zt63eGx-Sqaetb0eTpTmzYrgU",
+    authDomain: "tiktok-clone-1ec49.firebaseapp.com",
+    databaseURL: "https://tiktok-clone-1ec49.firebaseio.com",
+    projectId: "tiktok-clone-1ec49",
+    storageBucket: "tiktok-clone-1ec49.appspot.com",
+    messagingSenderId: "803267198496",
+    appId: "1:803267198496:web:0b45b7e53ea08ba27a4645",
+    measurementId: "G-DNTSCNVQGT"
+};
